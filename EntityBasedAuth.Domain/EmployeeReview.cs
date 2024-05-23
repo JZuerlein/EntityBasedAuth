41363a2bcbf0f9e85dccd9a6c204e84518045768
Jeff Zuerlein <Jeff@Zuerlein.com>
2024-05-22 20:56:43 -0500

    Add project files.

---
+++ EntityBasedAuth.Domain/EmployeeReview.cs
@@ -0,0 +1,18 @@
+﻿namespace EntityBasedAuth.Domain
+{
+    public class EmployeeReview
+    {
+        public int CreatorId { get; protected set; }
+        public int EmployeeId { get; protected set; }
+        public string EmployeeName { get; protected set; } = string.Empty;
+        public int Grade { get; protected set; }
+
+        public EmployeeReview(int creatorId, int employeeId, string employeeName, int grade)
+        {
+            CreatorId = creatorId;
+            EmployeeId = employeeId;
+            EmployeeName = employeeName;
+            Grade = grade;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+    {
+        options.LoginPath = new PathString("/Accunt/Login");
+        options.AccessDeniedPath = new PathString("/Account/Forbidden");
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy(EmployeeReviewPolicies.UserCanViewPolicyName,
+                      EmployeeReviewPolicies.GetUserCanViewPolicy());
+});
+
+builder.Services.AddSingleton<IAuthorizationRequirement,
+                              UserCanViewReviewRequirement>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
